ad7049354831e74e7c7365d6fff8a28b982e9f97
Ali Kiani <mr_kiani@yahoo.com>
2024-05-17 22:41:41 +0330

    fix config project and bind repositories and interfaces together

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,11 @@
 namespace App\Providers;
 
 use App\Interfaces\ArticleRepositoryInterface;
+use App\Interfaces\CommentRepositoryInterface;
+use App\Interfaces\LikeRepositoryInterface;
 use App\Repositories\ArticleRepository;
+use App\Repositories\CommentRepository;
+use App\Repositories\LikeRepository;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -14,6 +18,8 @@ class AppServiceProvider extends ServiceProvider
     public function register(): void
     {
         $this->app->bind(ArticleRepositoryInterface::class, ArticleRepository::class);
+        $this->app->bind(CommentRepositoryInterface::class, CommentRepository::class);
+        $this->app->bind(LikeRepositoryInterface::class, LikeRepository::class);
     }
 
     /**
